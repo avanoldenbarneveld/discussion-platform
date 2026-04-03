dae971fb087887dc6f2ae53c71d83cf28b14f66f
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2026-04-03 22:21:08 +0200

    feat: add react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './app.css'
+import { Route, Routes } from 'react-router-dom'
 import { posts } from './data/posts.js'
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
+
+function HomePage() {
   return (
     <main className="app">
       <h1>La Portada</h1>
@@ -10,7 +19,6 @@ function App() {
       </p>
 
       <section className="post-list">
-
         {posts.map((post) => (
           <article key={post.id} className="post-item">
             <h3 className="post-title">
@@ -23,7 +31,7 @@ function App() {
                 {post.source}
               </a>{' '}
               · {post.author} · {post.karma} karma · {post.timeAgo} ·{' '}
-              <a href={`/p/${post.id}`}>{post.commentCount} comentarios</a>
+              {post.commentCount} comentarios
             </p>
           </article>
         ))}
